e34776edf682a13b5d169bec4a990345b95d1af8
Daniel Adesoji <sojidaniels@gmail.com>
2022-09-03 20:04:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const mathM = require('./math')
+
+
+const PORT = 3000;
+
+const handleRequest = (req, res) => {
+    const {url, method } = req;
+    const body = [];
+    let result
+
+    if (url === '/calculate' && method === 'POST') {
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            const bodyObject = JSON.parse(parsedBody);
+
+            const { action, num1, num2 } = bodyObject
+
+            if (action === 'sum') {
+                result = mathM.add(num1, num2)
+            } else if (action === 'subtract') {
+                result = mathM.subtract(num1, num2)
+            } else if (action === 'divide') {
+                result = mathM.divide(num1, num2)
+            } else if (action === 'multiply') {
+                result = mathM.multiply(num1, num2)
+            }
+
+            res.end(JSON.stringify({ result }))
+        })
+
+        
+    }
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`)
+})
+
+module.exports = server;
---
+++ test/integration/app.test.js
@@ -0,0 +1,17 @@
+const supertest = require('supertest');
+const server = require('../../app');
+
+
+describe("Calculate", () => {
+    it('POST /calculate: action: sum', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'sum',
+            num1: 20,
+            num2: 10
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.body).toBe(JSON.stringify({ result: 30 }))
+    })
+})
