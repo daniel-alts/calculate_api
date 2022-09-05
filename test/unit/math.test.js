51cb8ba216e408acaca663bda64a1f92314fafef
Bukar Mamadu <bukarmamadu@gmail.com>
2022-09-05 18:36:26 +0100

    Made corrections all tests passed

--- test/unit/math.test.js
+++ test/unit/math.test.js
@@ -1,6 +1,6 @@
 const mathM = require('../../math');
 
-test.only('sum: expect 30', () => {
+test('sum: expect 30', () => {
     const actualResult = mathM.add(20, 10) 
     const expectResult = 30;
     expect(actualResult).toBe(expectResult)
@@ -12,19 +12,19 @@ test('sum: expect 45', () => {
     expect(actualResult).toBe(expectResult)
 });
 
-test('subtract', () => {
+test('subtract: expect 10', () => {
     const actualResult = mathM.subtract(20, 10) 
     const expectResult = 10;
     expect(actualResult).toBe(expectResult)
 });
 
-test('divide', () => {
+test('divide: expect 2', () => {
     const actualResult = mathM.divide(20, 10) 
     const expectResult = 2;
     expect(actualResult).toBe(expectResult)
 });
 
-test('multiply', () => {
+test('multiply: expect 200', () => {
     const actualResult = mathM.multiply(20, 10) 
     const expectResult = 200;
     expect(actualResult).toBe(expectResult)
