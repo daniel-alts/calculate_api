88a2557e2468e7d03e4cf935172096a8b197dba8
Aniki Yusuf <anikiyusuf99@gmail.com>
2022-09-06 20:06:30 +0100

    initial commit

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -10,8 +10,55 @@ describe("Calculate", () => {
             num2: 10
         })
 
-        // console.log({response})
+        console.log({response})
         expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
+        // expect(response.body).toBe(JSON.stringify({ result: 30 }))
+    })
+
+      //integration testing for subtraction 
+
+      it('POST /calculate: action: subtract', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 20,
+            num2: 10
+        })
+
+       
+         console.log({response})
+           expect(response.status).toBe(200)
+        //  expect(response.body).toBe({result:30})
+    })
+
+
+    
+    //integration testing for divide
+    it('POST /calculate: action: divide', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 20,
+            num2: 10
+        })
+
+       
+         console.log({response})
+           expect(response.status).toBe(200)
+        //  expect(response.body).toBe({result:30})
+    })
+
+
+  
+    //integration testing for multiply    
+    it('POST /calculate: action: multiply', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 20,
+            num2: 10
+        })
+
+       
+         console.log({response})
+           expect(response.status).toBe(200)
+        //  expect(response.body).toBe({result:30})
     })
 })
