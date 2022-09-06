7bfb61f5cc7642dba00c4410381186603a402400
st-pardon <okc4pardon@gmail.com>
2022-09-06 12:40:32 +0100

    updated test

--- test/unit/math.test.js
+++ test/unit/math.test.js
@@ -1,31 +1,31 @@
-const mathM = require('../../math');
+const { add, subtract, divide, multiply } = require('../../math');
 
-test.only('sum: expect 30', () => {
-    const actualResult = mathM.add(20, 10) 
+test('sum: expect 30', () => {
+    const actualResult = add(20, 10) 
     const expectResult = 30;
     expect(actualResult).toBe(expectResult)
 });
 
 test('sum: expect 45', () => {
-    const actualResult = mathM.add(15, 30)
+    const actualResult = add(15, 30)
     const expectResult = 45;
     expect(actualResult).toBe(expectResult)
 });
 
 test('subtract', () => {
-    const actualResult = mathM.subtract(20, 10) 
+    const actualResult = subtract(20, 10) 
     const expectResult = 10;
     expect(actualResult).toBe(expectResult)
 });
 
 test('divide', () => {
-    const actualResult = mathM.divide(20, 10) 
+    const actualResult = divide(20, 10) 
     const expectResult = 2;
     expect(actualResult).toBe(expectResult)
 });
 
 test('multiply', () => {
-    const actualResult = mathM.multiply(20, 10) 
+    const actualResult = multiply(20, 10) 
     const expectResult = 200;
     expect(actualResult).toBe(expectResult)
 });
