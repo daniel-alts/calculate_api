0dfb319152a0d6d106d80ae13ce641e285f7bd13
AdesounDeji <HP@Ghostman.(none)>
2022-09-06 15:54:49 +0100

    All test passed

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const http = require('http');
-const mathM = require('./math')
+const http = require("http");
+const mathM = require("./math")
 
 
 const PORT = 3000;
 
 const handleRequest = (req, res) => {
-    const {url, method } = req;
+    const { url, method } = req;
     const body = [];
     let result
 
-    if (url === '/calculate' && method === 'POST') {
-        req.on('data', (chunk) => {
+    if (url === "/calculate" && method === "POST") {
+        req.on("data", (chunk) => {
             body.push(chunk);
         });
 
-        req.on('end', () => {
+        req.on("end", () => {
             const parsedBody = Buffer.concat(body).toString()
             const bodyObject = JSON.parse(parsedBody);
 
             const { action, num1, num2 } = bodyObject
 
-            if (action === 'sum') {
+            if (action === "sum") {
                 result = mathM.add(num1, num2)
-            } else if (action === 'subtract') {
+            } else if (action === "subtract") {
                 result = mathM.subtract(num1, num2)
-            } else if (action === 'divide') {
+            } else if (action === "divide") {
                 result = mathM.divide(num1, num2)
-            } else if (action === 'multiply') {
+            } else if (action === "multiply") {
                 result = mathM.multiply(num1, num2)
             }
 
             res.end(JSON.stringify({ result }))
         })
 
-        
+
     }
 }
 
