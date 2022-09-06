4503b6d851e4e8e2832873909acec9efebae61d3
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-09-06 12:09:07 +0100

    Test Passed

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
-const http = require('http');
-const mathM = require('./math')
+const http = require("http");
+const mathM = require("./math");
 
-
-const PORT = 3000;
+const PORT = 3002;
 
 const handleRequest = (req, res) => {
-    const {url, method } = req;
-    const body = [];
-    let result
+  const { url, method } = req;
+  const body = [];
+  let result;
 
-    if (url === '/calculate' && method === 'POST') {
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
+  if (url === "/calculate" && method === "POST") {
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
 
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString()
-            const bodyObject = JSON.parse(parsedBody);
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const bodyObject = JSON.parse(parsedBody);
 
-            const { action, num1, num2 } = bodyObject
+      const { action, num1, num2 } = bodyObject;
 
-            if (action === 'sum') {
-                result = mathM.add(num1, num2)
-            } else if (action === 'subtract') {
-                result = mathM.subtract(num1, num2)
-            } else if (action === 'divide') {
-                result = mathM.divide(num1, num2)
-            } else if (action === 'multiply') {
-                result = mathM.multiply(num1, num2)
-            }
+      if (action === "sum") {
+        result = mathM.add(num1, num2);
+      } else if (action === "subtract") {
+        result = mathM.subtract(num1, num2);
+      } else if (action === "divide") {
+        result = mathM.divide(num1, num2);
+      } else if (action === "multiply") {
+        result = mathM.multiply(num1, num2);
+      } else if (action === "mean") {
+        result = mathM.mean(num1, num2);
+      }
 
-            res.end(JSON.stringify({ result }))
-        })
-
-        
-    }
-}
+      res.end(JSON.stringify({ result }));
+    });
+  }
+};
 
 const server = http.createServer(handleRequest);
 
 server.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`)
-})
+  console.log(`Server is listening on port: ${PORT}`);
+});
 
 module.exports = server;
--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -1,17 +1,46 @@
-const supertest = require('supertest');
-const server = require('../../app');
-
+const supertest = require("supertest");
+const server = require("../../app");
 
 describe("Calculate", () => {
-    it('POST /calculate: action: sum', async () => {
-        const response = await supertest(server).post('/calculate').send({
-            action: 'sum',
-            num1: 20,
-            num2: 10
-        })
+  it("POST /calculate: action: sum", async () => {
+    const response = await supertest(server).post("/calculate").send({
+      action: "sum",
+      num1: 20,
+      num2: 10,
+    });
 
-        // console.log({response})
-        expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
-    })
-})
+    // console.log({ response });
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(JSON.stringify({ result: 30 }));
+  });
+
+  it("POST /calculaate: action:divide", async () => {
+    const response = await supertest(server).post("/calculate").send({
+      action: "divide",
+      num1: 20,
+      num2: 10,
+    });
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(JSON.stringify({ result: 2 }));
+  });
+
+  it("POST /calculaate: action:multiply", async () => {
+    const response = await supertest(server).post("/calculate").send({
+      action: "multiply",
+      num1: 15,
+      num2: 10,
+    });
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(JSON.stringify({ result: 150 }));
+  });
+
+  it("POST /calculaate: action:subtract", async () => {
+    const response = await supertest(server).post("/calculate").send({
+      action: "subtract",
+      num1: 20,
+      num2: 10,
+    });
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(JSON.stringify({ result: 10 }));
+  });
+});
