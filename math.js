355c7a3914912bb43b94cfea276f4bf2017dcec6
Onyedikachi Onu <53058358+St-Pardon@users.noreply.github.com>
2022-09-06 09:47:35 +0000

    completed tasks

--- math.js
+++ math.js
@@ -2,10 +2,12 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const divide = (a, b) => a / b;
 const multiply = (a, b) => a * b;
+const remainder = (a, b) => a % b;
 
 module.exports = {
-    add,
-    subtract,
-    divide,
-    multiply,
+  add,
+  subtract,
+  divide,
+  multiply,
+  remainder,
 }
