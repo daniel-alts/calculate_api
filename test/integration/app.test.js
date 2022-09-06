ae8ca1f691c37ae5d2c2e57cb88a975937a22b0a
Ibeh Esther <estheribeh5@gmail.com>
2022-09-06 21:03:54 +0100

    test: Add integration test for subtract, divide, and multiply

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -8,10 +8,44 @@ describe("Calculate", () => {
             action: 'sum',
             num1: 20,
             num2: 10
-        })
+        });
+        // The API uses res.end(), hence no data is attached in the response body
 
-        // console.log({response})
-        expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
+        response.body = response.text;
+        expect(response.status).toBe(200);
+        expect(response.body).toBe(JSON.stringify({ result: 30 }));
+    })
+    it('POST /calculate: action: subtract', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 20,
+            num2: 10
+        })
+        // The API uses res.end(), hence no data is attached in the response body
+        response.body = response.text;
+        expect(response.status).toBe(200);
+        expect(response.body).toBe(JSON.stringify({ result: 10 }));
+    })
+    it('POST /calculate: action: multiply', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 20,
+            num2: 10
+        })
+        // The API uses res.end(), hence no data is attached in the response body
+        response.body = response.text;
+        expect(response.status).toBe(200);
+        expect(response.body).toBe(JSON.stringify({ result: 200 }));
+    })
+    it('POST /calculate: action: divide', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 20,
+            num2: 10
+        })
+        // The API uses res.end(), hence no data is attached in the response body
+        response.body = response.text;
+        expect(response.status).toBe(200);
+        expect(response.body).toBe(JSON.stringify({ result: 2 }));
     })
 })
