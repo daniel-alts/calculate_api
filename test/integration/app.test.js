bd9b325a3f63e101eee53aaa2fd20f8e6cf07fa1
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-07 01:36:23 +0100

    app.test.js

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -3,7 +3,7 @@ const server = require('../../app');
 
 
 describe("Calculate", () => {
-    it('POST /calculate: action: sum', async () => {
+    it('sends a POST request to /calculate endpoint and carrys out the action: sum', async () => {
         const response = await supertest(server).post('/calculate').send({
             action: 'sum',
             num1: 20,
@@ -12,6 +12,42 @@ describe("Calculate", () => {
 
         // console.log({response})
         expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
+        expect(response.text).toBe(JSON.stringify({ result: 30 }))
+    })
+
+    it('sends a POST request to /calculate endpoint and carrys out the action: sum', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 2,
+            num2: 3
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 6 }))
+    })
+
+    it('sends a POST request to /calculate endpoint and carrys out the action: subtract', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 30,
+            num2: 10
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 20 }))
+    })
+
+    it('sends a POST request to /calculate endpoint and carrys out the action: sum', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 28,
+            num2: 2
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 14 }))
     })
 })
