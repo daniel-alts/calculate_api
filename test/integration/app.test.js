1dc7ea4886625f32ee096f32972e8b7e08101b12
Emmanuel Eni <eejeba@gmail.com>
2022-09-07 02:00:04 +0100

    __1:58 wed sep7 wk36 2022__ I've been able to fix both the unit and integration tests.

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -1,17 +1,16 @@
 const supertest = require('supertest');
 const server = require('../../app');
 
+describe('Calculate', () => {
+  it('POST /calculate: action: sum', async () => {
+    const response = await supertest(server).post('/calculate').send({
+      action: 'sum',
+      num1: 20,
+      num2: 10,
+    });
 
-describe("Calculate", () => {
-    it('POST /calculate: action: sum', async () => {
-        const response = await supertest(server).post('/calculate').send({
-            action: 'sum',
-            num1: 20,
-            num2: 10
-        })
-
-        // console.log({response})
-        expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
-    })
-})
+    // console.log({response})
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(JSON.stringify({ result: 30 }));
+  });
+});
