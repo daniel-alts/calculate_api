9f1ff0429f4bc576683358a41b7dcf26e13654a6
Juliana Micheal-Echezona <julianamicheal06@gmail.com>
2022-09-07 20:48:05 +0100

    Update app.test.js

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -3,21 +3,6 @@ const server = require('../../app');
 
 
 describe("Calculate", () => {
-    it('POST /calculate: action: sum', async () => {
-        const response = await supertest(server).post('/calculate').send({
-            action: 'sum',
-            num1: 20,
-            num2: 10
-        })
-
-        // console.log({response})
-        expect(response.status).toBe(200)
-        expect(response.text).toBe(JSON.stringify({ result: 30 }))
-    })
-})
-
-
-
 it('POST /calculate: action: sum', async () => {
     const response = await supertest(server).post('/calculate').send({
         action: 'sum',
