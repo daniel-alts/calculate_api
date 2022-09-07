474caf9caba3fec6dec9acd56270c14204bb7618
Ikuesiri Onosemuode <ikuesiri.onosemuode@gmail.com>
2022-09-07 19:06:41 +0100

    I made corrections and also added more test cases to the unit and integration files

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -3,6 +3,7 @@ const server = require('../../app');
 
 
 describe("Calculate", () => {
+    //test for addition
     it('POST /calculate: action: sum', async () => {
         const response = await supertest(server).post('/calculate').send({
             action: 'sum',
@@ -12,6 +13,45 @@ describe("Calculate", () => {
 
         // console.log({response})
         expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
+        expect(response.text).toBe(JSON.stringify({ result: 30 }))
+    })
+
+    //test for substraction
+    it('POST /calculate: action: subtract', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 20,
+            num2: 10
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 10 }))
+    })
+
+    //test for division
+    it('POST /calculate: action: divide', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 20,
+            num2: 10
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 2 }))
+    })
+
+    //test for multiplication
+    it('POST /calculate: action: multiply', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 20,
+            num2: 10
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 200 }))
     })
 })
