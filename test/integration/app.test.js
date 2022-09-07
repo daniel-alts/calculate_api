c5142a40f722afde29338531d8fbc3dca610926f
Efunkunle Mukhtar <efunkunleolabode@gmail.com>
2022-09-07 23:30:55 +0100

    fixed and improved tests

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -14,3 +14,33 @@ describe("Calculate", () => {
     expect(response.text).toBe(JSON.stringify({ result: 30 }));
   });
 });
+
+it("POST /calculate that (20 - 10 = 10)", async () => {
+  const response = await supertest(server).post("/calculate").send({
+    action: "subtract",
+    num1: 20,
+    num2: 10,
+  });
+  expect(response.status).toBe(200);
+  expect(response.text).toBe(JSON.stringify({ result: 10 }));
+});
+
+it("POST /calculate that (20 / 10 = 2)", async () => {
+  const response = await supertest(server).post("/calculate").send({
+    action: "divide",
+    num1: 20,
+    num2: 10,
+  });
+  expect(response.status).toBe(200);
+  expect(response.text).toBe(JSON.stringify({ result: 2 }));
+});
+
+it("POST /calculate that (20 * 10 = 200)", async () => {
+  const response = await supertest(server).post("/calculate").send({
+    action: "multiply",
+    num1: 20,
+    num2: 10,
+  });
+  expect(response.status).toBe(200);
+  expect(response.text).toBe(JSON.stringify({ result: 200 }));
+});
