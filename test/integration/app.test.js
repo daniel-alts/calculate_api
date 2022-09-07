48bd610a5856453b859de1c7d5c67f9b0bc316ae
Frank-dev20 <franknjoe2@gmail.com>
2022-09-07 13:34:46 +0100

    Subtract, divide, and Multiply tests added and all passed

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -8,10 +8,40 @@ describe("Calculate", () => {
             action: 'sum',
             num1: 20,
             num2: 10
-        })
+        });
 
-        // console.log({response})
-        expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
-    })
-})
+         //console.log({response})
+        expect(response.status).toBe(200);
+        expect(response.text).toBe(JSON.stringify({ result: 30 }));
+        
+    });
+
+    it('POST /calculate: action: subtract', async()=>{
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 20,
+            num2: 5,
+        });
+        expect(response.status).toBe(200);
+        expect(response.text).toBe(JSON.stringify({ result: 15 }));
+    });
+
+    it('POST /calculate: action: divide', async()=>{
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 10,
+            num2: 2,
+        });
+        expect(response.status).toBe(200);
+        expect(response.text).toBe(JSON.stringify({ result: 5 }));
+    });
+     it('POST /calculate: action: multiply', async ()=>{
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 10,
+            num2: 10,
+        });
+        expect(response.status).toBe(200);
+        expect(response.text).toBe(JSON.stringify({ result: 100 }));
+    });
+});
