d100c34f0b65115c32b1d41aacb0545f03960ee5
Paul Solomon <paultsolomon@gmail.com>
2022-09-07 02:17:13 +0000

    done

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -3,15 +3,64 @@ const server = require('../../app');
 
 
 describe("Calculate", () => {
-    it('POST /calculate: action: sum', async () => {
+
+    //integration test for addition
+    it('POST /calculate: action: sum', async() => {
         const response = await supertest(server).post('/calculate').send({
-            action: 'sum',
+            action: "sum",
             num1: 20,
             num2: 10
         })
 
-        // console.log({response})
+        console.log({ response })
         expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
+        expect(response.text).toBe(JSON.stringify({ result: 30 }))
+    })
+
+    //integration test for subtraction 
+
+    it('POST /calculate: action: subtract', async() => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 20,
+            num2: 10
+        })
+
+
+        console.log({ response })
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 10 }))
+    })
+
+
+
+    //integration test for divide
+    it('POST /calculate: action: divide', async() => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 20,
+            num2: 10
+        })
+
+
+        console.log({ response })
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 2 }))
+    })
+
+
+
+    //integration test for multiply    
+    it('POST /calculate: action: multiply', async() => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 20,
+            num2: 10
+        })
+
+
+        console.log({ response })
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 200 }))
     })
 })
