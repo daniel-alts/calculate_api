9d90cce17d72b99e211834f8853896f322551eee
Osunronbi Okikiola <osunronbiolanlesi@gmail.com>
2022-09-07 23:33:13 +0100

    made first test to work

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -1,17 +1,16 @@
-const supertest = require('supertest');
-const server = require('../../app');
-
+const supertest = require("supertest");
+const server = require("../../app");
 
 describe("Calculate", () => {
-    it('POST /calculate: action: sum', async () => {
-        const response = await supertest(server).post('/calculate').send({
-            action: 'sum',
-            num1: 20,
-            num2: 10
-        })
-
-        // console.log({response})
-        expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
-    })
-})
+  it("POST /calculate: action: sum", async () => {
+    const response = await supertest(server).post("/calculate").send({
+      action: "sum",
+      num1: 20,
+      num2: 10,
+    });
+    const expectedResult = JSON.stringify({ result: 30 });
+    // console.log(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ result: 30 });
+  });
+});
