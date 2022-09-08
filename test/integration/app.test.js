356f658281998c043f208d7af41e8f18cc409254
sobiaya <scottobiaya@yahoo.co.uk>
2022-09-08 19:09:35 +0100

    added function test

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -1,5 +1,6 @@
 const supertest = require('supertest');
 const server = require('../../app');
+const { divide, multiply } = require('../../math');
 
 
 describe("Calculate", () => {
@@ -16,4 +17,43 @@ describe("Calculate", () => {
         expect(JSON.stringify(x)).toBe(JSON.stringify({result:30}))
         // expect(response.body).toBe(JSON.stringify({result: 30}));
     })
-})
+
+    describe('Calculate', ()=>{
+        it('post/calculate', async()=>{
+            const response = await supertest(server).post('/calculate').send({
+                action: 'subtract',
+                num1: 40,
+                num2: 30
+    
+            })
+    expect(response.status).toBe(200)
+    const x = response.body
+    expect(JSON.stringify(x)).toBe(JSON.stringify({result:10}))
+        })
+
+        describe('calculate/post', ()=>{
+            it('calculate/post', async ()=>{
+                const response = await supertest(server).post('/calculate').send({
+                    action: 'multiply',
+                    num1: 1,
+                    num2: 20
+                })
+                const x = response.body
+                expect(JSON.stringify(x)).toBe(JSON.stringify({result:20}))
+            })
+        })
+    })
+
+
+    describe('divid/calculate', ()=>{
+        it('post/cal', async()=>{
+            const response = await supertest(server).post('/calculate').send({
+                action: 'divide',
+                num1 : 6,
+                num2 : 3
+            })
+            const x = response.body
+            expect(JSON.stringify(x)).toBe(JSON.stringify({result:2}))
+        })
+    })
+    })
