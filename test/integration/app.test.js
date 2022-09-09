212133a1fa34846685257b34131b0c836376a578
Victor Abolarin <iam.victor@yahoo.co.uk>
2022-09-09 13:15:35 +0100

    Write test for multiply arithmetic

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -31,6 +31,20 @@ describe("Calculate", () => {
 })
 
 describe("Calculate", () => {
+    it('POST /calculate: action: multiply', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 20,
+            num2: 10
+        });
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 200 }))
+    })
+})
+
+describe("Calculate", () => {
     it('POST /calculate: action: equalTo', async () => {
         const response = await supertest(server).post('/calculate').send({
             action: 'equalTo',
