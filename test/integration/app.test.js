67b0af99d617842c1d9fae4b81e5c0d69e611dbd
Abayomi <abiodunabayomi91@gmail.com>
2022-09-09 21:35:43 +0100

    initialized an app.test.js

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -4,14 +4,58 @@ const server = require('../../app');
 
 describe("Calculate", () => {
     it('POST /calculate: action: sum', async () => {
-        const response = await supertest(server).post('/calculate').send({
-            action: 'sum',
-            num1: 20,
-            num2: 10
-        })
+        const response = await supertest(server).post('/calculate').send(
+            {
+                action: 'sum',
+                num1: 20,
+                num2: 10
+            }
+        );
 
         // console.log({response})
         expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
-    })
-})
+        expect(response.text).toBe(JSON.stringify({result:30}))
+    });
+
+    it('POST /calculate: action: divide', async () => {
+        const response = await supertest(server).post('/calculate').send(
+            {
+                action: 'divide',
+                num1: 200,
+                num2: 50
+            }
+        );
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({result:4}))
+    });
+
+    it('POST /calculate: action: multiply', async () => {
+        const response = await supertest(server).post('/calculate').send(
+            {
+               action: 'multiply',
+               num1: 20,
+               num2: 40
+            }
+        );
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({result:800}))
+    });
+
+    it('POST /calculate: action: subtract', async () => {
+        const response = await supertest(server).post('/calculate').send(
+            {
+                action: 'subtract',
+                num1: 45,
+                num2: 15
+           }
+        );
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({result:30}))
+    });
+});
