df10800d2c71623653e6a3fcd31b6e899033cb19
Michael Adegboye <olanrewaju443@gmail.com>
2022-09-09 19:28:43 +0100

    integration testing for division, multiplication and subtraction.

--- test/unit/math.test.js
+++ test/unit/math.test.js
@@ -6,26 +6,28 @@ test.only('sum: expect 30', () => {
     expect(actualResult).toBe(expectResult)
 });
 
-test('sum: expect 45', () => {
-    const actualResult = mathM.add(15, 30)
+test.only('sum: expect 45', () => {
+    const actualResult = mathM.add(30, 15) 
     const expectResult = 45;
     expect(actualResult).toBe(expectResult)
 });
 
+
 test('subtract', () => {
-    const actualResult = mathM.subtract(20, 10) 
-    const expectResult = 10;
+    const actualResult = mathM.subtract(50, 10) 
+    const expectResult = 40;
     expect(actualResult).toBe(expectResult)
 });
 
 test('divide', () => {
-    const actualResult = mathM.divide(20, 10) 
-    const expectResult = 2;
+    const actualResult = mathM.divide(80, 2) 
+    const expectResult = 40;
     expect(actualResult).toBe(expectResult)
 });
 
 test('multiply', () => {
-    const actualResult = mathM.multiply(20, 10) 
-    const expectResult = 200;
+    const actualResult = mathM.multiply(20, 20) 
+    const expectResult = 400;
     expect(actualResult).toBe(expectResult)
 });
+
