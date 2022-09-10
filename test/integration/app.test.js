d74dc6f596a6d9c0ab77a8d39ca8c080d1136e21
Ann-tech <Onyekaann17@gmail.com>
2022-09-10 14:00:15 +0100

    added integration test for subtract, divide and multiply

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -14,4 +14,35 @@ describe("Calculate", () => {
         expect(response.status).toBe(200)
         expect(response.text).toBe(JSON.stringify({ result: 30 }))
     })
+
+    it('POST /calculate: action: subtract', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 30,
+            num2: 15
+        })
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 15 }))
+    })
+
+    it('POST /calculate: action: divide', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 12,
+            num2: 4
+        })
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 3 }))
+    })
+
+    it('POST /calculate: action: multiply', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 12,
+            num2: 5
+        })
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 60 }))
+    })
+
 })
