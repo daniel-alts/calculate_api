0b23820f429a12375884084eaf1bc2e5060a759e
Osamuyi <muyixone@yahoo.com>
2022-09-10 15:41:18 +0100

    Osamuyi's calcAPI

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -3,15 +3,48 @@ const server = require('../../app');
 
 
 describe("Calculate", () => {
-    it('POST /calculate: action: sum', async () => {
+    it('POST /calculate: action: add', async () => {
         const response = await supertest(server).post('/calculate').send({
-            action: 'sum',
-            num1: 20,
-            num2: 10
+            "action": 'add',
+            "num1": 20,
+            "num2": 10
         })
+        expect(response.status).toBe(200);
+        expect(response.body.result).toBe(30);
+        
+    });
 
-        // console.log({response})
-        expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
-    })
+    it('POST /calculate: action: multiply', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            "action": 'multiply',
+            "num1": 8,
+            "num2": 100
+        })
+        expect(response.status).toBe(200);
+        expect(response.body.result).toBe(800);
+        
+    });
+
+    it('POST /calculate: action: divide', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            "action": 'divide',
+            "num1": 100,
+            "num2": 2
+        })
+        expect(response.status).toBe(200);
+        expect(response.body.result).toBe(50);
+        
+    });
+
+    it('POST /calculate: action: subtract', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            "action": 'subtract',
+            "num1": 8,
+            "num2": 100
+        })
+        expect(response.status).toBe(200);
+        expect(response.body.result).toBe(-92);
+        
+    });
+    
 })
