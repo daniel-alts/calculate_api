e6fea21f927d960cc910371f063f7f4a841d8dbe
Kasibel <belbosime@yahoo.co.uk>
2022-09-10 10:02:16 +0100

    modified app.js, app.test.js, math.test.js

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -10,8 +10,44 @@ describe("Calculate", () => {
             num2: 10
         })
 
-        // console.log({response})
+        console.log(response)
         expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
+        expect(response.text).toBe(JSON.stringify({ result: 30 }))
+    })
+
+    it('POST /calculate: action: subtract', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 50,
+            num2: 20
+        })
+
+        console.log(response)
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 30 }))
+    })
+
+    it('POST /calculate: action: multiply', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 15,
+            num2: 10
+        })
+
+        console.log(response)
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 150 }))
+    })
+
+    it('POST /calculate: action: divide', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 200,
+            num2: 10
+        })
+
+        console.log(response)
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({ result: 20 }))
     })
 })
