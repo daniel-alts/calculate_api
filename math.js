e620adecaf80bc1ff3a6846d04cad690b1f78f99
Rapheal Ajiboye <raphooo95@gmail.com>
2022-09-10 06:57:19 +0100

    setting up integration testing for subtraction, division and multiplication

--- math.js
+++ math.js
@@ -1,7 +1,7 @@
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const divide = (a, b) => a / b;
-const multiply = (a, b) => a * b;
+const add = (a, b) => parseInt(a) + parseInt(b);
+const subtract = (a, b) => parseInt(a) - parseInt(b);
+const divide = (a, b) => parseInt(a) / parseInt(b);
+const multiply = (a, b) => parseInt(a) * parseInt(b);
 
 module.exports = {
     add,
