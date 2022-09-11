559e9dd251508939b1f57677f26f5e68c2af085f
Eloghosa Morgan <eloghosawomen@gmail.com>
2022-09-11 01:25:20 +0100

    initial commit

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -15,3 +15,45 @@ describe("Calculate", () => {
         expect(response.body).toBe(JSON.stringify({ result: 30 }))
     })
 })
+
+describe("Calculate", () => {
+    it('POST /calculate: action: divide', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 20,
+            num2: 10
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.body).toBe(JSON.stringify({ result: 2 }))
+    })
+})
+
+describe("Calculate", () => {
+    it('POST /calculate: action: multiply', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 20,
+            num2: 10
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.body).toBe(JSON.stringify({ result: 200 }))
+    })
+})
+
+describe("Calculate", () => {
+    it('POST /calculate: action: subtract', async () => {
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 20,
+            num2: 10
+        })
+
+        // console.log({response})
+        expect(response.status).toBe(200)
+        expect(response.body).toBe(JSON.stringify({ result: 10 }))
+    })
+})
