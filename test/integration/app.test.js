1e21e81151de9f71fd5d4dd525d512b7793771e7
Oyinlolatechie <Adepitanoyinlola4@gmail.com>
2022-09-15 17:32:18 +0100

    Ran unit and integration test and ensured all test suites  passed

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -12,6 +12,49 @@ describe("Calculate", () => {
 
         // console.log({response})
         expect(response.status).toBe(200)
-        expect(response.body).toBe(JSON.stringify({ result: 30 }))
+        expect(response.text).toBe(JSON.stringify({ result: 30 }))
+    })
+
+    it('Should Subtarct Num1 from Num1, when POST /calculate: action== subtract', async()=>{
+        const response = await supertest(server).post('/calculate').send({
+            action: 'subtract',
+            num1: 50,
+            num2: 20
+        })
+
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({result: 30}))
+
+
+    })
+
+    
+    it('Should divide Num1 by Num2, when POST /calculate: action== divide', async()=>{
+        const response = await supertest(server).post('/calculate').send({
+            action: 'divide',
+            num1: 100,
+            num2: 20
+        })
+
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({result: 5}))
+
+
+    })
+
+
+    it('Should multiply  Num1 with Num2, when POST /calculate: action== multiply', async()=>{
+        const response = await supertest(server).post('/calculate').send({
+            action: 'multiply',
+            num1: 10,
+            num2: 20
+        })
+
+        expect(response.status).toBe(200)
+        expect(response.text).toBe(JSON.stringify({result: 200}))
+
+
     })
 })
+
+
