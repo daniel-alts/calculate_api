c5bc879e3ac65edb30aa352382d756177f787cf7
Abdussalam Mujeeb-ur-rahman <mjbabdussalam@gmail.com>
2022-10-23 01:46:10 +0100

    fix: added integration test for subtract, multiply and divide and made sure the server run smoothly with no interruption

--- app.js
+++ app.js
@@ -1,46 +1,61 @@
-const http = require('http');
-const mathM = require('./math')
-
+const http = require("http");
+const mathM = require("./math");
 
 const PORT = 3000;
 
-const handleRequest = (req, res) => {
-    const {url, method } = req;
-    const body = [];
-    let result
-
-    if (url === '/calculate' && method === 'POST') {
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString()
-            const bodyObject = JSON.parse(parsedBody);
-
-            const { action, num1, num2 } = bodyObject
-
-            if (action === 'sum') {
-                result = mathM.add(num1, num2)
-            } else if (action === 'subtract') {
-                result = mathM.subtract(num1, num2)
-            } else if (action === 'divide') {
-                result = mathM.divide(num1, num2)
-            } else if (action === 'multiply') {
-                result = mathM.multiply(num1, num2)
-            }
-
-            res.end(JSON.stringify({ result }))
-        })
-
-        
-    }
+function catchError() {
+  res.writeHead(400).end(
+    JSON.stringify({
+      message: error,
+    })
+  );
 }
+
+const handleRequest = (req, res) => {
+  const { url, method } = req;
+  const body = [];
+  let result;
+
+  if (url === "/calculate" && method === "POST") {
+    try {
+      req.on("data", (chunk) => {
+        body.push(chunk);
+      });
+      req.on("end", () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const bodyObject = JSON.parse(parsedBody);
+
+        const { action, num1, num2 } = bodyObject;
+        try {
+          if (action === "sum") {
+            result = mathM.add(num1, num2);
+          } else if (action === "subtract") {
+            result = mathM.subtract(num1, num2);
+          } else if (action === "divide") {
+            result = mathM.divide(num1, num2);
+          } else if (action === "multiply") {
+            result = mathM.multiply(num1, num2);
+          } else {
+            res.end("action not added!");
+          }
+
+          res.setHeader("Content-Type", "application/json");
+          res.writeHead(200);
+          res.end(JSON.stringify({ result }));
+        } catch (error) {
+          catchError;
+        }
+      });
+    } catch (error) {
+      catchError;
+    }
+  }
+};
 
 const server = http.createServer(handleRequest);
 
 server.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`)
-})
+  console.log(`Server is listening on port: ${PORT}`);
+});
 
 module.exports = server;
